1fdea4c88f1bd53c430b18b2393bfedb37f9ee39
Om-Govindani <omgovindani@gmail.com>
2024-09-02 06:54:43 +0530

    adding backend link

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:8080",
-  baseURL: "vector-jobs-api.vercel.app",
+  baseURL: "https://vector-jobs-api.vercel.app/",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
